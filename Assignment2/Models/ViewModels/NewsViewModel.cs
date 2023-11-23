248bae18c71ed477c54f3d4a007d8eb4e01a60ee
fuji0014 <fuji0014@algonquinlive.com>
2023-11-22 23:07:55 -0500

    Completed migration and mvc scaffolding successfully

---
+++ Assignment2/Models/ViewModels/NewsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment2.Models.ViewModels
+{
+    public class NewsViewModel
+    {
+        public IEnumerable<Fan> Fans { get; set; }
+        public IEnumerable<SportClub> SportClubs { get; set; }
+        public IEnumerable<Subscription> Subscriptions { get; set; }
+    }
+}
